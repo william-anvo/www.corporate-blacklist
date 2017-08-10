097e9743a44011b3533722681d237a63c396a7d0
william.t <william.t@192.168.198.129>
2017-08-10 11:08:09 +0800

    Merge origin/master into V3
    Pagnation enhancement
    Conflicts:
    	app/Http/Controllers/Blacklist/CustBlacklistController.php
    	resources/views/session/custBlacklisting.blade.php

--- app/Http/Controllers/Blacklist/CustBlacklistController.php
+++ app/Http/Controllers/Blacklist/CustBlacklistController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Blacklist;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use DB;
-use App\Quotation;
+//use App\Quotation;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 
 class CustBlacklistController extends Controller
 {
@@ -15,8 +17,13 @@ class CustBlacklistController extends Controller
 
     public function show() {
         
-        $blacklistings = DB::table('cust_blacklist')->where('deleted','0')->orderBy('id', 'asc')->get();
-        return view('session.custBlacklisting',compact('blacklistings'));
+        
+        $blacklistings = DB::table('cust_blacklist')->where('deleted','0')->orderBy('id', 'asc')->paginate(15);
+        
+        $blacklistings ->setPath('custblacklist');
+        
+        $skipped = ($blacklistings->currentPage() * $blacklistings->perPage()) - $blacklistings->perPage();
+        return view('session.custBlacklisting',compact(['blacklistings','skipped']));
 
     }
 }
